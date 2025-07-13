7e595fa9ce4ca8be2a89791926f6f660cc8c2b07
Daniel <mills.daniel320@gmail.com>
2025-07-13 17:49:22 -0500

    Added buttons in the html to click, added logic to determine winner of the game.

---
+++ script/script.js
@@ -0,0 +1,70 @@
+let userChoice = ''
+let gameArea = document.querySelector('main')
+const userOptions = gameArea.querySelectorAll('button')
+
+const rules = {
+    rock: {
+        beats: {
+            scissors: 'crushes',
+            lizard: 'crushes'
+        }
+    },
+    paper: {
+        beats: {
+            rock: 'covers',
+            spock: 'disproves'
+        }
+    },
+    scissors: {
+        beats: {
+            paper: 'cuts',
+            lizard: 'decapitates'
+        }
+    },
+    lizard: {
+        beats: {
+            paper: 'eats',
+            spock: 'poisons'
+        }
+    },
+    spock: {
+        beats: {
+            rock: 'vaporizes',
+            scissors: 'smashes'
+        }
+    }
+}
+
+userOptions.forEach(option => {
+  option.addEventListener('click', (e) => {
+    userChoice = e.currentTarget.value.toLowerCase()
+    const cpuChoice = computerChoice().toLowerCase()
+    const outcome = newCompareChoices(userChoice, cpuChoice)
+    console.log(outcome, userChoice, cpuChoice)
+  })
+})
+
+function computerChoice() {
+  let randomNumber = Math.floor(Math.random() * 10)
+  if (randomNumber <= 1) return 'Rock' 
+  else if (randomNumber <= 3) return 'Paper' 
+  else if (randomNumber <= 5) return 'Scissors'
+  else if (randomNumber <= 7) return 'Lizard'
+  else return 'Spock'
+}
+
+function newCompareChoices(userChoices, enemyChoices) {
+  if (userChoices === enemyChoices) return 'No One Wins!'
+  if (rules[userChoice].beats[enemyChoices]) {
+    const verb = rules[userChoice].beats[enemyChoices]
+    return `Player Wins! ${capitalize(userChoice)} ${verb} ${capitalize(enemyChoices)}!`
+  } 
+  else {
+    const verb = rules[enemyChoices].beats[userChoice]
+    return `Computer Wins! ${capitalize(enemyChoices)} ${verb} ${capitalize(userChoice)}!`
+  }
+}
+
+function capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1)
+}
