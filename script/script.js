dbe0d84c3d2b3a9da4da0eb882983dee90ae0f7c
Daniel <mills.daniel320@gmail.com>
2025-07-18 18:17:14 -0500

    made some minor name adjustments, started adding the play state ui

--- script/script.js
+++ script/script.js
@@ -1,5 +1,6 @@
 let userChoice = ''
-let gameArea = document.querySelector('section')
+let state = 'start'
+let gameArea = document.querySelector('.game-area-waiting')
 const userOptions = gameArea.querySelectorAll('button')
 
 const rules = {
@@ -38,9 +39,10 @@ const rules = {
 userOptions.forEach(option => {
   option.addEventListener('click', (e) => {
     userChoice = e.currentTarget.value.toLowerCase()
+    state = 'playing'
     const cpuChoice = computerChoice().toLowerCase()
     const outcome = newCompareChoices(userChoice, cpuChoice)
-    console.log(outcome, userChoice, cpuChoice)
+    console.log(outcome, userChoice, cpuChoice, state)
   })
 })
 
@@ -68,3 +70,15 @@ function newCompareChoices(userChoices, enemyChoices) {
 function capitalize(str) {
     return str.charAt(0).toUpperCase() + str.slice(1)
 }
+
+//When a player makes a choice we need to switch to the play state
+//The first step is to register the player's choice(already doing this logically), and change the ui
+//The second step is to have the opponent make their choice (also already doing)
+//Third we want it to feel like the opponent is thinking, so set some short delay
+//Fourth display the choice and display who won
+//Fifth, if the user won, update the score.
+//Sixth, display the play again state
+//Seventh, if play again is clicked, we start back at the og ui
+//Optional functionality: player stats stored in local storage
+//Stats would be saved score, the games that won, total times a certain option was chosen
+//Show user biases and which games they won the most with.
